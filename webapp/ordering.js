125956465d87d3715cc79758c5364366f3cb106b
LovetheFrogs <marcosferrerzalve@gmail.com>
2025-10-20 23:26:26 +0200

    Fix: Step arguments in insertionSort steps
    Updated the insertionSort function to consistently pass the index 'i' and 'j' instead of array values v[i] and v[j] to the Step constructor. This change improves clarity and consistency in the step-tracking logic.

--- webapp/ordering.js
+++ webapp/ordering.js
@@ -19,22 +19,22 @@ function insertionSort(v) {
     steps.push(new Step(0, null, null, null, null, null, 1, [...v]));
 
     for (let i = 1; i <= LAST; i++) {
-        steps.push(new Step(steps.length, v[i], steps[steps.length - 1].jVal, steps[steps.length - 1].auxVal, i, steps[steps.length - 1].jPos, 1, [...v]));
+        steps.push(new Step(steps.length, i, steps[steps.length - 1].jVal, steps[steps.length - 1].auxVal, i, steps[steps.length - 1].jPos, 1, [...v]));
         const aux = v[i];
-        steps.push(new Step(steps.length, v[i], steps[steps.length - 1].jVal, aux, i, steps[steps.length - 1].jPos, 2, [...v]));
+        steps.push(new Step(steps.length, i, steps[steps.length - 1].jVal, aux, i, steps[steps.length - 1].jPos, 2, [...v]));
         let j = i - 1;
-        steps.push(new Step(steps.length, v[i], v[j], aux, i, j, 3, [...v]));
+        steps.push(new Step(steps.length, i, j, aux, i, j, 3, [...v]));
 
         while (j >= 0 && v[j] > aux) {
-            steps.push(new Step(steps.length, v[i], v[j], aux, i, j, 4, [...v]));
+            steps.push(new Step(steps.length, i, j, aux, i, j, 4, [...v]));
             v[j + 1] = v[j];
-            steps.push(new Step(steps.length, v[i], v[j], aux, i, j, 5, [...v]));
+            steps.push(new Step(steps.length, i, j, aux, i, j, 5, [...v]));
             j--;
-            steps.push(new Step(steps.length, v[i], v[j], aux, i, j, 6, [...v]));
+            steps.push(new Step(steps.length, i, j, aux, i, j, 6, [...v]));
         }
 
         v[j + 1] = aux;
-        steps.push(new Step(steps.length, v[i], v[j], aux, i, j, 7, [...v]));
+        steps.push(new Step(steps.length, i, j, aux, i, j, 7, [...v]));
     }
 
     return steps;
